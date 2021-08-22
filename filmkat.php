423cb281c2f4cf384953ec6041fcfcc797e5f897
ahmtkzk <ahmetkazak08@gmail.com>
2021-08-23 01:09:13 +0300

    Movie list updated.

--- filmkat.php
+++ filmkat.php
@@ -1,13 +1,10 @@
 <?php
 include("settings/db.php");
 
-$FilmCekSorgu = $Baglanti->prepare("select * from filmler");
-$FilmCekSorgu->execute();
-$Filmler = $FilmCekSorgu->fetchAll(PDO::FETCH_ASSOC);
+$FilmCekSorguIlk3 = $Baglanti->prepare("select * from filmler order by id desc");
+$FilmCekSorguIlk3->execute();
+$Filmler = $FilmCekSorguIlk3->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($Filler as $Rows){
-    echo $Rows["filmadi"];
-}
 
 ?>
 <!-- İçerikler -->
@@ -20,7 +17,7 @@ foreach ($Filler as $Rows){
     <!--https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg-->
     <div class="row">
 
-        <div class="col-md-2 me-5 mt-3">
+        <div class="col-md-2 me-5 mt-3 position-relative">
             <nav class="nav flex-column border  bg-light mb-3">
                 <a class="nav-link disabled" href="#"><h5>Kategoriler</h5>
                     <hr>
@@ -33,126 +30,31 @@ foreach ($Filler as $Rows){
             </nav>
         </div>
 
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
+        <div class="col-md-9 mt-3">
+
+            <?php foreach ($Filmler as $Rows) { ?>
+
+                <div class="col-md-4 mb-3 float-start">
+                    <a href="#" class="alt-cizgisiz">
+                        <div class="card m-auto" style="width: 18rem;">
+                            <img src="<?php echo $Rows["poster"];?>"
+                                 class="card-img-top" style="height: 425px">
+                            <div class="card-footer bg-light">
+                                <div class="progress mt-1 mb-1">
+                                    <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
+                                         aria-valuemin="0" aria-valuemax="100">25%
+                                    </div>
+                                </div>
                             </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item fw-bold mt-2"><h5><?php echo $Rows["filmadi"];?></h5></li>
+                            </ul>
                         </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
+                    </a>
                 </div>
-            </a>
-        </div>
 
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
-                </div>
-            </a>
-        </div>
+            <?php } ?>
 
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
-                </div>
-            </a>
-        </div>
-
-    </div>
-
-    <div class="row mt-3">
-
-        <div class="col-md-2 me-5">
-
-        </div>
-
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
-                </div>
-            </a>
-        </div>
-
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
-                </div>
-            </a>
-        </div>
-
-        <div class="col-md-3 mt-3">
-            <a href="#" class="alt-cizgisiz">
-                <div class="card m-auto" style="width: 18rem;">
-                    <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ZB3EBbF3HHywU7jfcwP8eDnUwn.jpg"
-                         class="card-img-top" alt="...">
-                    <div class="card-footer bg-light">
-                        <div class="progress mt-1 mb-1">
-                            <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25"
-                                 aria-valuemin="0" aria-valuemax="100">25%
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item fw-bold mt-2"><h5>Film ismi</h5></li>
-                    </ul>
-                </div>
-            </a>
         </div>
 
     </div>
